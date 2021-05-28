09b3d5c3e29cb72cb5e6f9bc3a67f12c87b4f08c
emannuelvv <54732031+emannuelvv@users.noreply.github.com>
2021-05-28 17:26:19 -0300

    upando novamente com bug OK

---
+++ EscreverSms/EscreverSMS/SMS.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace EscreverSMS
+{
+    public class SMS
+    {
+        private string mensagem;
+
+        public SMS(string input)
+        {
+
+            mensagem = input;
+            ValidarTamanhoDaMensagem();
+
+        }
+
+        
+        public string SequenciaNumerica()
+        {
+            string strNumerosResultado = "";
+
+            string saidaAtual;
+
+            string proximaSaida;
+
+            for (int i = 0; i < mensagem.Length; i++)
+
+            {
+
+                strNumerosResultado += EscreverNumero(mensagem.Substring(i, 1));
+
+                if (i + 1 < mensagem.Length)
+
+                {
+
+                    saidaAtual = EscreverNumero(mensagem.Substring(i, 1)).Substring(0, 1);
+
+                    proximaSaida = EscreverNumero(mensagem.Substring(i + 1, 1)).Substring(0, 1);
+
+                    if (saidaAtual == proximaSaida)
+                        strNumerosResultado += "_";
+                }
+            }
+
+            return strNumerosResultado;
+        }
+
+        private string EscreverNumero(string letra)
+
+        {
+
+            string numero = "";
+
+            switch (letra.ToUpper())
+
+            {
+                case "A": numero = "2"; break;
+                case "B": numero = "22"; break;
+                case "C": numero = "222"; break;
+                case "D": numero = "3"; break;
+                case "E": numero = "33"; break;
+                case "F": numero = "333"; break;
+                case "G": numero = "4"; break;
+                case "H": numero = "44"; break;
+                case "I": numero = "444"; break;
+                case "J": numero = "5"; break;
+                case "K": numero = "55"; break;
+                case "L": numero = "555"; break;
+                case "M": numero = "6"; break;
+                case "N": numero = "66"; break;
+                case "O": numero = "666"; break;
+                case "P": numero = "7"; break;
+                case "Q": numero = "77"; break;
+                case "R": numero = "777"; break;
+                case "S": numero = "7777"; break;
+                case "T": numero = "8"; break;
+                case "U": numero = "88"; break;
+                case "V": numero = "888"; break;
+                case "W": numero = "9"; break;
+                case "X": numero = "99"; break;
+                case "Y": numero = "999"; break;
+                case "Z": numero = "9999"; break;
+                case " ": numero = "0"; break;
+                default: numero = letra; break;
+            }
+
+            return numero;
+        }
+
+        private void ValidarTamanhoDaMensagem()
+
+        {
+            if (mensagem.Length > 255)
+
+                throw new ArgumentOutOfRangeException("A mensagem devera ter no máximo 255 caracteres!!!!!!!!");
+        }
+    }
+}
---
+++ EscreverSms/Tests/SMSTests.cs
@@ -0,0 +1,34 @@
+﻿using EscreverSMS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class SMSTests
+    {
+        [TestMethod]
+        public void DeveSerInputValidoUppercase()
+        {
+            SMS sms = new SMS("");
+
+            Assert.AreEqual("", sms.SequenciaNumerica());
+        }
+
+        [TestMethod]
+        public void DeveSerInputValidoLowercase()
+        {
+            SMS sms = new SMS("");
+
+            Assert.AreEqual("", sms.SequenciaNumerica());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NaoDeveSerInputValido()
+        {
+            _ = new SMS(""
+                );
+        }
+    }
+}
